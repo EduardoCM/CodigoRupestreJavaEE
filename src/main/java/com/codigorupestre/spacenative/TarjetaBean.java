e61d15e69c4627925f1b9268235f7efeeea76c18
EduardoCM <ecm3198@gmail.com>
2020-06-05 20:34:24 -0500

    first commit

---
+++ src/main/java/com/codigorupestre/spacenative/TarjetaBean.java
@@ -0,0 +1,16 @@
+package com.codigorupestre.spacenative;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author edcastillo
+ */
+@Named
+@RequestScoped
+public class TarjetaBean {
+
+	
+}
